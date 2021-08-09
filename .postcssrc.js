78ead1223b81b0598e840a79eda7df9c11a79a45
chaeyeon kim <kcy85078507@gmail.com>
2021-08-09 10:42:32 +0900

    first-bundler

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+// ESM
+// CommonJS 노드
+
+// import autoprefixer from 'autoprefixer'
+// export {
+//    plugins: [
+//       autoprefixer
+//    ]
+// }
+const autoprefixer = require("autoprefixer");
+module.exports = {
+  plugins: [autoprefixer],
+};
